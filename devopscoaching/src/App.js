492fb25b0ef091d11a7b825a97a44a1ee9939154
mcyenikoylu <mcy@dotnetmcy.net>
2022-10-29 14:36:42 +0300

    create react app

---
+++ devopscoaching/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello, DevOps Coaching
+        </p>
+        <a
+          className="App-link"
+          href="https://devopsengineering.circle.so/s/devopsengineers-community/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click for Comminity!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
